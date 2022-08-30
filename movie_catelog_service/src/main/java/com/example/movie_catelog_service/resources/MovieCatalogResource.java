4eda5ce453832336a9cac979337a1f392ce5c989
CB007753 <cb007753@students.apiit.lk>
2022-08-30 12:08:03 +0530

    07. Using WebClient to make API calls

--- movie_catelog_service/src/main/java/com/example/movie_catelog_service/resources/MovieCatalogResource.java
+++ movie_catelog_service/src/main/java/com/example/movie_catelog_service/resources/MovieCatalogResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 
 import java.util.Arrays;
@@ -20,6 +21,9 @@ public class MovieCatalogResource {
 
     @Autowired
     private RestTemplate restTemplate;
+
+    @Autowired
+    private WebClient.Builder webClientBuilder;
 
 //Creating a dummy api
     @RequestMapping("/{userId}")
@@ -35,8 +39,21 @@ public class MovieCatalogResource {
 
 
         return ratings.stream().map(rating -> {
+            //Below line is used with RestTemplate to get an instance of movie
+           // Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
 
-            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
+            //Below code will give an instance of movie using Web Client Builder
+            // * webClientBuilder.build() is using a builder pattern and giving you a client
+            // * .get() is telling the type of method user wants(here user wants to get an instance of movie).. another method is .post() which is used to post data
+            // * .uri() is used to specify the url that you need to access
+            // * .retrieve() is used to ask to fetch data from specified url
+            // * bodyToMono() is used to convert whatever the body that .retrieve() gets, into an instance of Movie.class
+            Movie movie = webClientBuilder.build()
+                    .get()
+                    .uri("http://localhost:8082/movies/"+rating.getMovieId())
+                    .retrieve()
+                    .bodyToMono(Movie.class)
+                    .block();
 
            return new CatalogItem(movie.getName(), "Desc", rating.getRating());
         }).collect(Collectors.toList());
