1f57092ac10b747b6e7be4affec88fc355e7b985
CB007753 <cb007753@students.apiit.lk>
2022-09-15 14:42:15 +0530

    11. Discovering services using Eureka (With the use of @LoadBalanced annotation in MovieCatelogServiceApplication.java and mentioning the client's registered name instead of actual url in MovieCatalogResource)

--- movie_catelog_service/src/main/java/com/example/movie_catelog_service/resources/MovieCatalogResource.java
+++ movie_catelog_service/src/main/java/com/example/movie_catelog_service/resources/MovieCatalogResource.java
@@ -29,12 +29,12 @@ public class MovieCatalogResource {
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
 
-        UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/"+userId, UserRating.class);
+        UserRating ratings = restTemplate.getForObject("http://rating-data-service/ratingsdata/users/"+userId, UserRating.class);
 
         return ratings.getUserRating().stream().map(rating -> {
 
             //For each movie Id, call movie info service and get details
-           Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
+           Movie movie = restTemplate.getForObject("http://movie-info-service/movies/"+rating.getMovieId(), Movie.class);
 
            //Put them all together
            return new CatalogItem(movie.getName(), "Desc", rating.getRating());
